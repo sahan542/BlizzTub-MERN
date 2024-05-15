a7cb2373b34ea05f8fe9b415855aca2b94bf1206
sahan542 <sahanrashmika542@gmail.com>
2024-05-15 12:54:54 +0530

    user model & userSignup added to backend

--- Back-End/controller/userSignup.js
+++ Back-End/controller/userSignup.js
@@ -1,5 +1,38 @@
+const userModel = require("../models/userModel")
+const bcrypt = require('bcryptjs');
+
 async function userSignUpController(req,res){
     try{
+        const { email, password, name} = req.body
+        if(!email){
+            throw new Error("Please Provide Email")
+        }
+        if(!password){
+            throw new Error("Please Provide Password")
+        }
+        if(!name){
+            throw new Error("Please Provide Name")
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(password, salt);
+
+        if(!hashPassword){
+            throw new Error("Something is wrong")
+        }
+        const payload = {
+            ...req.body,
+            password : hashPassword
+        }
+        const userData = new userModel(payload)
+        const saveUser = userData.save()
+
+        res.status(201).json({
+            data : saveUser,
+            success : true,
+            error : false,
+            message : "User Created Successfully"
+        })
 
     }
     catch(err){
@@ -10,3 +43,5 @@ async function userSignUpController(req,res){
         })
     }
 }
+
+    module.exports = userSignUpController
