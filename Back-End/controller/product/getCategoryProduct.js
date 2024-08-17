13f742c8ecf3d0f2bfe8f553c0aca0db85dfd707
sahan542 <sahanrashmika542@gmail.com>
2024-08-17 14:54:20 +0530

    get product from each category

--- Back-End/controller/product/getCategoryProduct.js
+++ Back-End/controller/product/getCategoryProduct.js
@@ -6,6 +6,22 @@ const getCategoryProductController = async(req,res)=>{
     try{
         const productCategory = await productModel.distinct("category")
         console.log("category - ",productCategory)
+
+        //array to store one product from each category
+        const productByCategory = []
+        for(const category of productCategory){
+            const product = await productModel.findOne({ category })
+
+            if(product){
+                productByCategory.push(category)
+            }
+        }
+        res.json({
+            message: "Category product",
+            data: productCategory,
+            success: true,
+            error: false
+        })
     }
     catch(error){
         res.status(400).json({
